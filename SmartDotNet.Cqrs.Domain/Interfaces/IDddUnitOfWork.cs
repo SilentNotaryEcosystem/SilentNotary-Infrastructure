4b63b982f324e28a938446dad9e6c5da6004b6af
Igor Kuznetsov <mr.snk51@gmail.com>
2017-12-25 00:54:18 +0300

    IAggregateFactory and IDddUnitOfWork interfaces; CommandHandlerBase class

---
+++ SmartDotNet.Cqrs.Domain/Interfaces/IDddUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+
+namespace SmartDotNet.Cqrs.Domain.Interfaces
+{
+    public interface IDddUnitOfWork
+    {
+        AggregateRepository<TAggregate, TId> Repository<TAggregate, TId>()
+            where TAggregate : AggregateRoot<TId>;
+
+        IValueObjectProvider ValueObjectProvider { get; }
+
+        Task<Result> CommitAsync();
+    }
+}
---
+++ SmartDotNet.Cqrs/Commands/CommandHandlerBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using SmartDotNet.Cqrs.Domain;
+using SmartDotNet.Cqrs.Domain.Interfaces;
+
+namespace SmartDotNet.Cqrs.Commands
+{
+    public abstract class CommandHandlerBase<TAggregate, TKey>
+        where TAggregate: AggregateRoot<TKey>
+    {
+        protected readonly IDddUnitOfWork UnitOfWork;
+        protected readonly AggregateRepository<TAggregate, TKey> Repository;
+
+        public CommandHandlerBase(IDddUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+            Repository = UnitOfWork.Repository<TAggregate, TKey>();
+        }
+
+        protected Task<Result> SaveChanges(TAggregate employee)
+        {
+            Repository.Update(employee);
+            return UnitOfWork.CommitAsync();
+        }
+    }
+}
