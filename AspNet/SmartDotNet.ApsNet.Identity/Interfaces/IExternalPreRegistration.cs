031e08b470d3e1f83255e7137693476f37b5c1d7
Igor Kuznetsov <mr.snk51@gmail.com>
2018-01-26 16:18:43 +0300

    external preregistration

---
+++ AspNet/SmartDotNet.ApsNet.Identity/Interfaces/IExternalPreRegistration.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartDotNet.ApsNet.Identity.Interfaces
+{
+    public interface IExternalPreRegistration
+    {
+        string Provider { get; set; }
+
+        string Sid { get; set; }
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+    }
+}
---
+++ AspNet/SmartDotNet.ApsNet.Identity/Interfaces/IExternalPreRegistrationStore.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace SmartDotNet.ApsNet.Identity.Interfaces
+{
+    public interface IExternalPreRegistrationStore<TPreRegistration> where TPreRegistration: IExternalPreRegistration
+    {
+        Task AddPreRegistrationAsync(TPreRegistration preRegistration);
+        Task<TPreRegistration> GetPreRegistrationAsync(string provider, string sid);
+    }
+}
