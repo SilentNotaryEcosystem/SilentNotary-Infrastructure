d1d5e04e0a37fafd6dc5ae9625a08592c98a1717
Igor Kuznetsov <mr.snk51@gmail.com>
2018-05-04 19:28:01 +0300

    generic specifications

---
+++ SmartDotNet.Cqrs/GenericSpecification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+using SmartDotNet.Specifications;
+
+namespace SmartDotNet.Cqrs
+{
+    public class GenericSpecification<T> : Specification<T>
+    {
+        private readonly Expression<Func<T, bool>> _predicate;
+
+        internal GenericSpecification(Expression<Func<T, bool>> predicate)
+        {
+            _predicate = predicate ?? throw new ArgumentException(nameof(predicate));
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            return _predicate;
+        }
+    }
+}
---
+++ SmartDotNet.Cqrs/GenericSpecifications.cs
@@ -0,0 +1,19 @@
+﻿using SmartDotNet.Cqrs.Domain.Interfaces;
+using SmartDotNet.Specifications;
+
+namespace SmartDotNet.Cqrs
+{
+    public static class GenericSpecifications
+    {
+        public static Specification<TEntity> All<TEntity>()
+        {
+            return new GenericSpecification<TEntity>(entity => true);
+        }
+
+        public static Specification<TEntity> WithId<TEntity, TKey>(TKey id)
+            where TEntity : IHasKey<TKey>
+        {
+            return new GenericSpecification<TEntity>(entity => entity.Id.Equals(id));
+        }
+    }
+}
